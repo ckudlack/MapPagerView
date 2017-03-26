e4e151dd69e0784f4c016ab680e00e7985c46741
ckudlack <thebigcheese40@gmail.com>
2017-03-26 10:53:35 -0700

    Updated libs

--- library/src/main/java/com/cdk/bettermapsearch/interfaces/SelectedItemCallback.java
+++
@@ -1,7 +0,0 @@
-package com.cdk.bettermapsearch.interfaces;
-
-import android.support.annotation.Nullable;
-
-public interface SelectedItemCallback<T extends MapClusterItem> {
-    @Nullable T getSelectedItem();
-}
