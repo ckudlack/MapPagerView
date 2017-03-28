72bedaf2a30ed902b48ed68c4ae5b77155925582
ckudlack <thebigcheese40@gmail.com>
2017-03-28 10:22:57 -0700

    Removed wildcard from comparator

--- library/src/main/java/com/cdk/bettermapsearch/MapClusterItemComparator.java
+++ library/src/main/java/com/cdk/bettermapsearch/MapClusterItemComparator.java
@@ -6,16 +6,16 @@ import com.google.maps.android.clustering.Cluster;
 
 import java.util.Comparator;
 
-public class MapClusterItemComparator implements Comparator<Cluster<? extends MapClusterItem>> {
+public class MapClusterItemComparator implements Comparator<Cluster<MapClusterItem>> {
 
-    private Cluster<? extends MapClusterItem> currentLocation;
+    private Cluster<MapClusterItem> currentLocation;
 
-    public MapClusterItemComparator(Cluster<? extends MapClusterItem> currentLocation) {
+    public MapClusterItemComparator(Cluster<MapClusterItem> currentLocation) {
         this.currentLocation = currentLocation;
     }
 
     @Override
-    public int compare(Cluster<? extends MapClusterItem> cluster1, Cluster<? extends MapClusterItem> cluster2) {
+    public int compare(Cluster<MapClusterItem> cluster1, Cluster<MapClusterItem> cluster2) {
         final double distance1 = SphericalUtil.computeDistanceBetween(currentLocation.getPosition(), cluster1.getPosition());
         final double distance2 = SphericalUtil.computeDistanceBetween(currentLocation.getPosition(), cluster2.getPosition());
 
